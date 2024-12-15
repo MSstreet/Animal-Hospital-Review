fd21ca734eeac19aafed8abec2c004747f8862af
MSstreet <cortm5pp@gmail.com>
2024-12-15 22:34:41 +0900

    first commit

---
+++ src/main/java/com/toy/pet/animal_hospital_review/AnimalHospitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.toy.pet.animal_hospital_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHospitalReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalHospitalReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/pet/animal_hospital_review/AnimalHospitalReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.pet.animal_hospital_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHospitalReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
