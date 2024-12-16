29d91be880b9b0654e50044d721b41fa045ad3db
MSstreet <cortm5pp@gmail.com>
2024-12-17 07:18:44 +0900

    commit

---
+++ src/main/java/com/toy/pet/animal_hospital_review/entity/BoardRepository.java
@@ -0,0 +1,6 @@
+package com.toy.pet.animal_hospital_review.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+}
