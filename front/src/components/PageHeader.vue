0779488aa76432086e5337fd27499109dcc5ebfb
MSstreet <cortm5pp@gmail.com>
2024-12-19 07:20:20 +0900

    로그인 기능 구현 중

--- front/src/components/PageHeader.vue
+++ front/src/components/PageHeader.vue
@@ -1,19 +1,17 @@
-<!-- PageHeader.vue -->
 <template>
   <header>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/board/list">게시판</router-link>
+      <router-link to="/board/list">게시판</router-link> |
+      <router-link to="/login">로그인</router-link>
     </div>
   </header>
   <hr/>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
